afd99f292a23559986327c1630a5595b2631d967
amy3374 <119571479+amy3374@users.noreply.github.com>
2023-04-20 15:31:26 +0900

    라우터 설정 완료

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import {Outlet} from "react-router-dom"
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <Outlet />
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from "./pages/Home"
+import Plan from './pages/Plan';
+import MyPlan from './pages/MyPlan';
+import MyPlanDetail from './pages/MyPlanDetail';
+
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element : <App />,
+    children : [{
+      index : true,
+      element: <Home />,
+
+  },
+  {
+    path : "plan",
+    element: <Plan />,
+
+},
+{
+  path : "myplan",
+  element: <MyPlan />,
+
+},
+{
+  path : "myplandetail",
+  element: <MyPlanDetail />,
+
+}]
+  }
+])
+
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router = {router}/>
+  </React.StrictMode>
+);
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
